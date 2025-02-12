a96e941a7b5437d45af71c08c22994ad87c04229
Chevee Ferrer <cferrerdesign@gmail.com>
2025-02-12 15:59:13 +0800

    Updated port

--- ocr-api.py
+++ ocr-api.py
@@ -75,6 +75,10 @@ def get_cropped_image():
 #     app.config["TEMPLATES_AUTO_RELOAD"] = True
 #     app.run(host="0.0.0.0", port=5000, debug=True)
 
+@app.route("/")
+def home():
+    return "OCR API is running!"
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Use Render's PORT
+    port = int(os.environ.get("PORT", 5000))  # Use PORT from Render, default to 5000 for local
     app.run(host="0.0.0.0", port=port)
