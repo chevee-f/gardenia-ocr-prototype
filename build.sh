0d35fe4e3f513cac9d510ac713f43ae27603c75a
Chevee Ferrer <cferrerdesign@gmail.com>
2025-02-13 04:37:32 +0800

    deploy fixes

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-export PATH=$PATH:/usr/local/python3.9.19/bin
-export PATH="/opt/render/project/poetry/bin:$PATH"
 pip install --upgrade pip
-pip install git+https://github.com/benoitc/gunicorn.git
 
 # Install system dependencies
 apt-get update && apt-get install -y \
@@ -12,6 +9,3 @@ apt-get update && apt-get install -y \
 
 # Install Python dependencies
 pip install -r requirements.txt
-echo "--echoing path START--"
-echo $PATH
-echo "--echoing path END--"
