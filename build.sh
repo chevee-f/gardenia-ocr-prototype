2502ca5d2a87fec561a0209d923adfc306cec807
Chevee Ferrer <cferrerdesign@gmail.com>
2025-02-13 05:14:37 +0800

    added sudo

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 pip install --upgrade pip
 
 # Install system dependencies
-apt-get update && apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
     tesseract-ocr \
     libtesseract-dev \
     poppler-utils
